3399de45470a855a39546217a11d0945b2fdca81
senthil kumar <senthilpk@ymail.com>
2021-07-26 08:46:35 +0530

    fix(FB-9364): Get latest changes from upstream

---
+++ lib/jsonld/event.d.ts
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+import { Address, AggregateOffer, Offers } from '../types';
+declare type Location = {
+  name: string;
+  address: Address;
+  sameAs?: string;
+};
+declare type Performer = {
+  name: string;
+};
+export interface EventJsonLdProps {
+  keyOverride: string;
+  name: string;
+  startDate: string;
+  endDate: string;
+  location: Location;
+  url?: string;
+  description?: string;
+  images?: string[];
+  offers?: Offers | Offers[];
+  aggregateOffer?: AggregateOffer;
+  performers?: Performer | Performer[];
+}
+declare const EventJsonLd: FC<EventJsonLdProps>;
+export default EventJsonLd;
